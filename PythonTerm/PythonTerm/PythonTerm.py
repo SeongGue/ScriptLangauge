dd01239759043d5a4ae39f68ef406fe6672bc98f
songseulki <thdtmfrl1245@naver.com>
2017-05-26 15:59:50 +0900

    텀프로젝트
    메뉴 만들었음

--- PythonTerm/PythonTerm/PythonTerm.py
+++ PythonTerm/PythonTerm/PythonTerm.py
@@ -6,6 +6,9 @@ import base64
 import codecs
 from xml.dom.minidom import *
 
+loop = True
+api_key = "pXzMF6%2Fw76xCHSzsvA1g%2BW5SFbBIk2dWoUF91PDp%2FN3UvF84Wq0io0KPhbT9VyE9Z7o9YgiUGqsMrCTfp55Fvw%3D%3D"
+
 def addParsingDicList(xmlData, motherData, childData):
     # ?Ľ̵? ?????͸? ????Ʈ?? ?־ ???? ?Ѵ?.
     doc = parseString(xmlData)
@@ -27,45 +30,62 @@ def openAPItoXML(server, key, value):
     data = urllib.request.urlopen(req).read().decode('utf-8')
     return data
 
+def localSearch():
+    server2 = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey="
+    value2 = "&MobileOS=ETC&MobileApp=AppTesting&numOfRows=1000"
+    
+    
+    
+    realdata = openAPItoXML(server2, api_key, value2)
+    
+    localList = addParsingDicList(realdata, "item", "name")
+    localListNum = addParsingDicList(realdata, "item", "code")
+    
+    print(localList)
+    #print(localListNum)
+    
+    local = input("지역을 적어주세요 : ")
+    
+    for i in localList:
+        if ( local == i ):
+            local = localListNum[localList.index(i)]
+    
+    #print(local)
+    server2 = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival?ServiceKey="
+    value2 = str("&areaCode=") + str(local) + str("&MobileOS=ETC&MobileApp=AppTesting&contentTypeId=15")
+    
+    #print(value2)
+    
+    realdata = openAPItoXML(server2, api_key, value2)
+    
+    #print(realdata)
+    eventLocal = addParsingDicList(realdata, "item", "addr1")
+    eventName = addParsingDicList(realdata, "item", "title")
+    eventTel = addParsingDicList(realdata, "item", "tel")
+    eventStartDate = addParsingDicList(realdata, "item", "eventstartdate")
+    eventEndDate = addParsingDicList(realdata, "item", "eventenddate")
+    eventImage = addParsingDicList(realdata, "item", "firstimage")
+    
+    for i in eventLocal:
+            print(str("\nName : ") + eventName[eventLocal.index(i)] + str("\nLocal : ") + i + str("\nTel : ") + eventTel[eventLocal.index(i)] +  str("\nStarDate : ") + eventStartDate[eventLocal.index(i)] + str("\nEndDate : ") + eventEndDate[eventLocal.index(i)] + str  ("\nImage : ") + eventImage[eventLocal.index(i)] + str("\n") )
+    
 
-server2 = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode?ServiceKey="
-key2 = "pXzMF6%2Fw76xCHSzsvA1g%2BW5SFbBIk2dWoUF91PDp%2FN3UvF84Wq0io0KPhbT9VyE9Z7o9YgiUGqsMrCTfp55Fvw%3D%3D"
-value2 = "&MobileOS=ETC&MobileApp=AppTesting&numOfRows=1000"
+while (loop == True):
+    print("------------------------------------------------------------------")
+    print("1. 지역검색")
+    print("2. 텍스트 검색")
+    print("3. 종료")
+    print("------------------------------------------------------------------")
+    key_num = input("번호를 입력해주세요 : ")
 
+    if int(key_num) == 1:
+        localSearch()
+    
 
+    if int(key_num) == 2:
+        print("텍스트로 검새합니다.")
 
-realdata = openAPItoXML(server2, key2, value2)
-
-localList = addParsingDicList(realdata, "item", "name")
-localListNum = addParsingDicList(realdata, "item", "code")
-
-print(localList)
-print(localListNum)
-
-print("Local List : ")
-local = input()
-
-for i in localList:
-    if ( local == i ):
-        local = localListNum[localList.index(i)]
-
-print(local)
-server2 = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival?ServiceKey="
-value2 = str("&areaCode=") + str(local) + str("&MobileOS=ETC&MobileApp=AppTesting&contentTypeId=15")
-
-#print(value2)
-
-realdata = openAPItoXML(server2, key2, value2)
-
-
-#print(realdata)
-eventLocal = addParsingDicList(realdata, "item", "addr1")
-eventName = addParsingDicList(realdata, "item", "title")
-eventTel = addParsingDicList(realdata, "item", "tel")
-eventStartDate = addParsingDicList(realdata, "item", "eventstartdate")
-eventEndDate = addParsingDicList(realdata, "item", "eventenddate")
-eventImage = addParsingDicList(realdata, "item", "firstimage")
-
-for i in eventLocal:
-        print(str("\nName : ") + eventName[eventLocal.index(i)] + str("\nLocal : ") + i + str("\nTel : ") + eventTel[eventLocal.index(i)] + str("\nStarDate : ") + eventStartDate[eventLocal.index(i)] + str("\nEndDate : ") + eventEndDate[eventLocal.index(i)] + str("\nImage : ") + eventImage[eventLocal.index(i)] + str("\n") )
+    if int(key_num) == 3:
+        loop = False;
+    
 
